cf5bbb451864943f8ee737f2e40ed70598f245f4
KhanSaquib <saquibk894@gmail.com>
2023-08-21 00:46:45 +0530

    added frontend

--- src/main/java/com/app/project/controller/UserController.java
+++ src/main/java/com/app/project/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("https://project-frontend-one.vercel.app/adduser")
 public class UserController {
     @Autowired
     UserRepository ur;
